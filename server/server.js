750badd906f233429292a15bbf12742ff3a7e9ba
Francisco Dimmer <ftdimmer@gmail.com>
2025-12-01 20:45:23 -0300

    conecto rutas de tareas con sus controladores

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
+const taskRoutes = require('./routes/taskRoutes');
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'API del Task Manager funcionando' });
