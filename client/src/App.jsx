64c0c6e4995c76b9ad37d135a70b0f7ce5b8ceba
Francisco Dimmer <ftdimmer@gmail.com>
2025-12-01 21:15:24 -0300

    estructura inicial del frontend con vite

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Task Manager</h1>
+      <p>Aplicación en construcción...</p>
+    </div>
+  );
+}
+
+export default App;
